73d8318b4fe03e82e6fdd8fbae7c5632a3d4e397
t_inoue <t.artwork.ceramica@gmail.com>
2023-10-27 08:29:04 +0900

    section4: H2データベースとMyBatis のセットアップ、テーブル作成と初期データの投入

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('バグA', 'バグがあります');
+insert into issues (summary, description) values ('機能要望B', 'Bに追加機能があります');
+insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
